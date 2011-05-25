7ab4568357cd81b8a40753ac546385e885ea2399
Dave Thomas <kukulcanenator@googlemail.com>
2011-05-25 01:42:46 +0100

    agent based pipelet checkin with semaphoreslim for bounding not sure i like this yet but we will see...

--- src/PipeletsDemo/Program.fs
+++ src/PipeletsDemo/Program.fs
@@ -1,61 +1,104 @@
-﻿module PipeletsDemo
-    open System
-    open Pipelets
+﻿module File1
+open System
+open System.Diagnostics
+open System.Threading
+open fracture
 
-    let split del n (s:string) = 
-        Console.WriteLine(sprintf "%s: Before split %A" n s)
-        let split = s.Split([|del|]) 
-        Console.WriteLine(sprintf "%s: After: split into: %A" n split)
-        split |> Array.toSeq
+let reverse (s:string) = 
+    new string(s |> Seq.toArray |> Array.rev)
 
-    let reverse (s:string) = 
-        new string(s |> Seq.toArray |> Array.rev)
+let oneToSingleton a b f=
+        //Console.WriteLine(sprintf "%s: Before reverse %A" a b)
+        let result = b |> f 
+        //Console.WriteLine(sprintf "%s: After reverse %A" a result)
+        result |> Seq.singleton
 
-    let oneToSingleton a b f=
-            Console.WriteLine(sprintf "%s: Before reverse %A" a b)
-            let result = b |> f 
-            Console.WriteLine(sprintf "%s: After reverse %A" a result)
-            result|> Seq.singleton
+///total number to run through test cycle
+let number = 10000
 
-    let OneToSeqRev a b = oneToSingleton a b reverse 
+///dirty hack to record when we are done
+let counter = ref 0
+let sw = new Stopwatch()
+let countthis (a:String) =
+    do Interlocked.Increment(counter) |> ignore
+    if !counter % number = 0 then 
+        sw.Stop()
+        printfn "Execution time: %A" sw.Elapsed
+    counter|> Seq.singleton
 
-    ///Simply picks the first route
-    let basicRouter( r, i) =
-        r|> Seq.head |> Seq.singleton
+let OneToSeqRev a b = oneToSingleton a b reverse 
 
-    let printoverflow stageno msg =
-        let str = sprintf"%s: Overflow: %A" stageno msg
-        Console.WriteLine(str)
+///Simply picks the first route
+let basicRouter( r, i) =
+    r|> Seq.head |> Seq.singleton
+              
+let stage1 = pipelet("Stage1", OneToSeqRev "1", basicRouter, number, -1)
+let stage2 = pipelet("Stage2", OneToSeqRev "2", basicRouter, number, -1)
+let stage3 = pipelet("Stage3", OneToSeqRev "3", basicRouter, number, -1)
+let stage4 = pipelet("Stage4", OneToSeqRev "4", basicRouter, number, -1)
+let stage5 = pipelet("Stage5", OneToSeqRev "5", basicRouter, number, -1)
+let stage6 = pipelet("Stage6", OneToSeqRev "6", basicRouter, number, -1)
+let stage7 = pipelet("Stage7", OneToSeqRev "7", basicRouter, number, -1)
+let stage8 = pipelet("Stage8", OneToSeqRev "8", basicRouter, number, -1)
+let stage9 = pipelet("Stage9", OneToSeqRev "9", basicRouter, number, -1)
+let stage10 = pipelet("Stage10", OneToSeqRev "10", basicRouter, number, -1)
+let stage11 = pipelet("Stage11", OneToSeqRev "11", basicRouter, number, -1)
+let stage12 = pipelet("Stage12", OneToSeqRev "12", basicRouter, number, -1)
+let stage13 = pipelet("Stage13", OneToSeqRev "13", basicRouter, number, -1)
+let stage14 = pipelet("Stage14", OneToSeqRev "14", basicRouter, number, -1)
+let stage15 = pipelet("Stage15", OneToSeqRev "15", basicRouter, number, -1)
+let stage16 = pipelet("Stage16", OneToSeqRev "16", basicRouter, number, -1)
+let stage17 = pipelet("Stage17", OneToSeqRev "17", basicRouter, number, -1)
+let stage18 = pipelet("Stage18", OneToSeqRev "18", basicRouter, number, -1)
+let stage19 = pipelet("Stage19", OneToSeqRev "19", basicRouter, number, -1)
+let stage20 = pipelet("Stage20", OneToSeqRev "20", basicRouter, number, -1)
+let final = pipelet("Final", countthis, basicRouter, number, -1)
+
+//construction is long winded as there are no symbolic ops yet...
+stage1.Attach(stage2)
+stage2.Attach(stage3)
+stage3.Attach(stage4)
+stage4.Attach(stage5)
+stage5.Attach(stage6)
+stage6.Attach(stage7)
+stage7.Attach(stage8)
+stage8.Attach(stage9)
+stage9.Attach(stage10)
+stage10.Attach(stage11)
+stage11.Attach(stage12)
+stage12.Attach(stage13)
+stage13.Attach(stage14)
+stage14.Attach(stage15)
+stage15.Attach(stage16)
+stage16.Attach(stage17)
+stage17.Attach(stage18)
+stage18.Attach(stage19)
+stage19.Attach(stage20)
+stage20.Attach(final)
+
+// note object expression to fake up last stage as no routing 
+// will take place if nothing is attached to final stage
+// at the moment this is an optimisation i.e. if there id nothing attached
+// then why transform, let alone route, nee to think about use cases on this....
+final.Attach(  {new IPipeletInput<_> with member this.Post payload = () })
+   
+let generateCircularSeq (lst:'a list) = 
+    let rec next () = 
+        seq {
+            for element in lst do
+                yield element
+            yield! next()
+        }
+    next()
     
-    let p1 = pipelet(split ',' "1", basicRouter, 5, printoverflow "1", 1000)
-    let p2 = pipelet(OneToSeqRev "2", basicRouter, 5, printoverflow "2", 1000)
-    let p3 = pipelet(OneToSeqRev "3", basicRouter, 5, printoverflow "3", 1000)
-    let p4 = pipelet(OneToSeqRev "4", basicRouter, 5, printoverflow "4", 1000)
-    let p5 = pipelet(OneToSeqRev "5", basicRouter, 5, printoverflow "5", 1000)
-    let p6 = pipelet(OneToSeqRev "6", basicRouter, 5, printoverflow "6", 1000)
-    let p7 = pipelet(OneToSeqRev "7", basicRouter, 5, printoverflow "7", 1000)
-    let p8 = pipelet(OneToSeqRev "8", basicRouter, 5, printoverflow "8", 1000)
-    let p9 = pipelet(OneToSeqRev "9", basicRouter, 5, printoverflow "9", 1000)
-    let p10 = pipelet(OneToSeqRev "10", basicRouter, 5, printoverflow "10", 1000)
+System.AppDomain.CurrentDomain.UnhandledException |> Observable.add (fun x -> 
+    printfn "%A" (x.ExceptionObject :?> Exception);Console.ReadKey() |>ignore)
 
-    //composition of stages
-    p1 ++> p2 ++> p3 ++> p4 ++> p5++> p6++> p7++> p8++> p9++> p10 |> ignore
+sw.Start()
+for str in ["John"; "Paul"; "George"; "Ringo"; "Nord"; "Bert"] 
+|> generateCircularSeq 
+|> Seq.take number
+    do  str --> stage1
 
-    let generateCircularSeq (lst:'a list) = 
-        let rec next () = 
-            seq {
-                for element in lst do
-                    yield element
-                yield! next()
-            }
-        next()
-    
-    System.AppDomain.CurrentDomain.UnhandledException |> Observable.add (fun x -> 
-        printfn "%A" (x.ExceptionObject :?> Exception);Console.ReadKey() |>ignore)
-
-    for str in ["John,Paul,George,Ringo"; "Nord,Bert"] 
-    |> generateCircularSeq 
-    |> Seq.take 10
-        do  str -->> p1
-
-    let x = Console.ReadKey()
+Console.WriteLine("Press a key to exit.")
+let x = Console.ReadKey()
