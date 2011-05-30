990268e6af2784da2229cf950c85231153d06b6d
Ryan Riley <ryan.riley@panesofglass.org>
2011-05-29 21:04:22 -0700

    Added initial parser (linear white space) for HTTP parsing.

---
+++ src/lib/httpparser/Http.fs
@@ -0,0 +1,34 @@
+﻿module HttpParser.Http
+
+open System
+open FParsec.Primitives
+open FParsec.CharParsers
+open HttpParser.Primitives
+open HttpParser.CharParsers
+open HttpParser.Uri
+
+type HttpRequestMessageParseEvent =
+  | RequestMessageBegin
+  | RequestMethod of char list
+  | RequestUri of char list
+  | QueryString of char list
+  | Fragment of char list
+  | RequestHeaderName of char list
+  | RequestHeaderValue of char list
+  | RequestHeadersEnd
+  | RequestBody of char list
+  | RequestMessageEnd
+
+type HttpResponseMessageParseEvent =
+  | ResponseMessageBegin
+  | StatusCode of char list
+  | StatusDescription of char list
+  | ResponseHeaderName of char list
+  | ResponseHeaderValue of char list
+  | ResponseHeadersEnd
+  | ResponseBody of char list
+  | ResponseMessageEnd
+
+// Basic Rules
+let lws<'a> : Parser<char, 'a> = (fun _ _ -> ' ') <!> opt newline <*> many1 (space <|> tab)
+  
---
+++ src/tests/HttpParser.Tests/HttpTest.fs
@@ -0,0 +1,42 @@
+﻿module HttpParser.Tests.HttpTest
+
+open System
+open FParsec.Error
+open FParsec.CharParsers
+open HttpParser.Primitives
+open HttpParser.CharParsers
+open HttpParser.Uri
+open HttpParser.Http
+open NUnit.Framework
+open FsUnit
+
+let run p input =
+  match run p input with
+  | Success(actual,_,_) -> actual
+  | Failure(error,_,_) -> failwith error
+
+[<TestCase(" ", Result=' ')>]
+[<TestCase("\t", Result=' ')>]
+[<TestCase("\n ", Result=' ')>]
+[<TestCase("\r ", Result=' ')>]
+[<TestCase("\r\n ", Result=' ')>]
+[<TestCase("\n  ", Result=' ')>]
+[<TestCase("\r  ", Result=' ')>]
+[<TestCase("\r\n  ", Result=' ')>]
+[<TestCase("\n\t", Result=' ')>]
+[<TestCase("\r\t", Result=' ')>]
+[<TestCase("\r\n\t", Result=' ')>]
+[<TestCase("\n\t\t", Result=' ')>]
+[<TestCase("\r\t\t", Result=' ')>]
+[<TestCase("\r\n\t\t", Result=' ')>]
+[<TestCase("\n \t", Result=' ')>]
+[<TestCase("\r \t", Result=' ')>]
+[<TestCase("\r\n \t", Result=' ')>]
+[<TestCase("\n\t ", Result=' ')>]
+[<TestCase("\r\t ", Result=' ')>]
+[<TestCase("\r\n\t ", Result=' ')>]
+[<TestCase("", ExpectedException=typeof<Exception>)>]
+[<TestCase("\n", ExpectedException=typeof<Exception>)>]
+[<TestCase("\r", ExpectedException=typeof<Exception>)>]
+[<TestCase("\r\n", ExpectedException=typeof<Exception>)>]
+let ``test lws should parse one or more white spaces, including an optional line fold``(input) = run lws input
