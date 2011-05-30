b068420647fdcd34718a219004ab0f1a7ad91970
Ryan Riley <ryan.riley@panesofglass.org>
2011-05-29 19:39:22 -0700

    Added common char parsers for uri and http parsers.

---
+++ src/lib/httpparser/CharParsers.fs
@@ -0,0 +1,21 @@
+﻿module HttpParser.CharParsers
+
+open System
+open FParsec.Primitives
+open FParsec.CharParsers
+open Primitives
+
+let control<'a> : Parser<char,'a> = anyOf (String(Array.map char [| 0uy..31uy |])) <|> pchar (char 127uy)
+let alphanum<'a> : Parser<char, 'a> = asciiLetter <|> digit
+let space<'a> : Parser<char, 'a> = pchar ' '
+let dquote<'a> : Parser<char, 'a> = pchar '"'
+let hash<'a> : Parser<char, 'a> = pchar '#'
+let percent<'a> : Parser<char, 'a> = pchar '%'
+let plus<'a> : Parser<char, 'a> = pchar '+'
+let hyphen<'a> : Parser<char, 'a> = pchar '-'
+let dot<'a> : Parser<char, 'a> = pchar '.'
+let colon<'a> : Parser<char, 'a> = pchar ':'
+let semicolon<'a> : Parser<char, 'a> = pchar ';'
+let slash<'a> : Parser<char, 'a> = pchar '/'
+let qmark<'a> : Parser<char, 'a> = pchar '?'
+let escaped<'a> : Parser<char, 'a> = readHex <!> skipChar '%' *> hex <*> hex
---
+++ src/tests/HttpParser.Tests/CharParsersTest.fs
@@ -0,0 +1,17 @@
+﻿module HttpParser.Tests.CharParsersTest
+
+open System
+open FParsec.CharParsers
+open HttpParser.Hex
+open HttpParser.Primitives
+open HttpParser.CharParsers
+open NUnit.Framework
+open FsUnit
+
+[<TestCase("%20"," ")>]
+[<TestCase("%2F","/")>]
+[<TestCase("%2f","/")>]
+let ``test escaped parser returns the byte representing the full hexadecimal character``(esc, expected) =
+  match run escaped esc with
+  | Success(actual,_,_) -> string actual |> should equal expected
+  | Failure(error,_,_) -> failwith error
