30978c52e04b468dd7e1b8cf839f9e8018e66ed6
Ryan Riley <ryan.riley@panesofglass.org>
2011-05-29 19:33:17 -0700

    Added additional parsing primitives.

---
+++ src/lib/httpparser/Primitives.fs
@@ -0,0 +1,22 @@
+﻿module HttpParser.Primitives
+
+open System
+open FParsec.Primitives
+open HttpParser.Hex
+
+module String =
+  let ofCharList (input:char list) = String(Array.ofList input)
+  let toCharList (input:String) = input.ToCharArray() |> Array.toList
+
+let inline (<*>) f a = f >>= fun f' -> a >>= fun a' -> preturn (f' a')
+let inline lift f a = a |>> f
+let inline (<!>) f a = lift f a
+let inline lift2 f a b = pipe2 a b f  // preturn f <*> a <*> b
+let inline ( *>) x y = x >>. y        // lift2 (fun _ z -> z) x y
+let inline ( <*) x y = x .>> y        // lift2 (fun z _ -> z) x y
+let inline ( !!) s = String.ofCharList s
+let readHex a b = fromHexDigit a <<< 4 ||| fromHexDigit b |> char
+let listify a = (fun x -> [x]) <!> a
+let listify2 a b = (fun a b -> a::[b]) <!> a <*> b
+let cons hd tl = hd::tl
+let flatten hd tl = hd::tl |> List.concat
---
+++ src/tests/HttpParser.Tests/PrimitivesTest.fs
@@ -0,0 +1,34 @@
+﻿module HttpParser.Tests.PrimitivesTest
+
+open System
+open FParsec.CharParsers
+open HttpParser.Hex
+open HttpParser.Primitives
+open NUnit.Framework
+open FsUnit
+
+[<TestCase('\n',"\n",'\n')>]
+[<TestCase('\r',"\r",'\r')>]
+let ``test pchar '\r' and '\n' should return '\n'``(charToParse, input, expected) =
+  let p = pchar charToParse
+  match run p input with
+  | Success(actual,_,_) -> actual |> should equal expected
+  | Failure(error,_,_) -> failwith error
+
+[<TestCase("\n",'\n')>]
+[<TestCase("\r",'\n')>]
+[<TestCase("\r\n",'\n')>]
+let ``test newline should parse all newline chars``(input, expected) =
+  match run newline input with
+  | Success(actual,_,_) -> actual |> should equal expected
+  | Failure(error,_,_) -> failwith error
+
+[<TestCase('a','b',"ab","ab")>]
+[<TestCase('/','/',"//","//")>]
+[<TestCase(' ',' ',"  ","  ")>]
+let ``test listify2 should run two parsers and put the results in a list``(x, y, input, expected) =
+  let expected = String.toCharList expected
+  let p = listify2 (pchar x) (pchar y)
+  match run p input with
+  | Success(actual,_,_) -> actual |> should equal expected
+  | Failure(error,_,_) -> failwith error
