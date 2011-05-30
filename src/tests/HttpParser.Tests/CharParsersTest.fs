8bdf1bc2427a0f422da7767336c47b72fb9ce123
Ryan Riley <ryan.riley@panesofglass.org>
2011-05-29 20:44:12 -0700

    Added additional char parser tests and extracted the common test runner pattern into a run function.

--- src/tests/HttpParser.Tests/CharParsersTest.fs
+++ src/tests/HttpParser.Tests/CharParsersTest.fs
@@ -8,10 +8,56 @@ open HttpParser.CharParsers
 open NUnit.Framework
 open FsUnit
 
-[<TestCase("%20"," ")>]
-[<TestCase("%2F","/")>]
-[<TestCase("%2f","/")>]
-let ``test escaped parser returns the byte representing the full hexadecimal character``(esc, expected) =
-  match run escaped esc with
-  | Success(actual,_,_) -> string actual |> should equal expected
+let run p input =
+  match run p input with
+  | Success(actual,_,_) -> actual
   | Failure(error,_,_) -> failwith error
+
+[<TestCase(" ", Result=' ')>]
+[<TestCase("0", ExpectedException=typeof<Exception>)>]
+let ``test space parser should parse a ' '``(input) = run space input
+
+[<TestCase("\"", Result='"')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test dquote parser should parse a '"'``(input) = run dquote input
+ 
+[<TestCase("#", Result='#')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test hash parser should parse a '#'``(input) = run hash input
+
+[<TestCase("%", Result='%')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test percent parser should parse a '%'``(input) = run percent input
+
+[<TestCase("+", Result='+')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test plus parser should parse a '+'``(input) = run plus input
+ 
+[<TestCase("-", Result='-')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test hyphen parser should parse a '-'``(input) = run hyphen input
+ 
+[<TestCase(".", Result='.')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test dot parser should parse a '.'``(input) = run dot input
+
+[<TestCase(":", Result=':')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test colon parser should parse a ':'``(input) = run colon input
+
+[<TestCase(";", Result=';')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test semicolon parser should parse a ';'``(input) = run semicolon input
+
+[<TestCase("/", Result='/')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test slash parser should parse a '/'``(input) = run slash input
+ 
+[<TestCase("?", Result='?')>]
+[<TestCase(" ", ExpectedException=typeof<Exception>)>]
+let ``test qmark parser should parse a '?'``(input) = run qmark input
+
+[<TestCase("%20", Result=' ')>]
+[<TestCase("%2F", Result='/')>]
+[<TestCase("%2f", Result='/')>]
+let ``test escaped parser returns the byte representing the full hexadecimal character``(input) = run escaped input
