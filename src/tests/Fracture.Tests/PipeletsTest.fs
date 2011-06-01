ee03b07c71843182140bceb2e711d1e94a2fc220
Ryan Riley <ryan.riley@panesofglass.org>
2011-06-01 06:55:22 -0700

    Resolved problem with unit tests.

--- src/tests/Fracture.Tests/PipeletsTest.fs
+++ src/tests/Fracture.Tests/PipeletsTest.fs
@@ -9,23 +9,41 @@ let singletonRouter messages (routes:'a IPipeletInput seq) =
     let route = Seq.head routes
     route.Post msg
 
-let counter = ref 0
-let finished = ref false
-let finisher msg =
-    incr counter
-    finished := true
-    Seq.singleton msg
-
-let finish = new Pipelet<int,int>("Finish", finisher, singletonRouter, 1, -1)
-
 [<Test>]
-[<Ignore>]
 let ``test should iterate once``() =
-    finish.Post 1
-    !counter |> should equal 1
+    let counter = ref 0
+    let finished = ref false
+    let finisher msg =
+        incr counter
+        finished := true
+        Seq.singleton msg
+    
+    let finish = new Pipelet<int,int>("Finish", finisher, singletonRouter, 1, -1)
+    finish ++> { new IPipeletInput<_> with member this.Post payload = () } |> ignore
+
+    async {
+        finish.Post 1
+        // Give the post a chance to complete
+        do! Async.Sleep 500
+        return !counter |> should equal 1
+    } |> Async.RunSynchronously
 
 [<Test>]
-[<Ignore>]
 let ``test should finish after one iteration``() =
-    finish.Post 1
-    !finished |> should be True
+    let counter = ref 0
+    let finished = ref false
+    let finisher msg =
+        incr counter
+        finished := true
+        Seq.singleton msg
+    
+    let finish = new Pipelet<int,int>("Finish", finisher, singletonRouter, 1, -1)
+    finish ++> { new IPipeletInput<_> with member this.Post payload = () } |> ignore
+
+    async {
+        finish.Post 1
+        // Give the post a chance to complete
+        do! Async.Sleep 500
+        return !finished |> should be True
+    } |> Async.RunSynchronously
+    
