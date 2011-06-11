983bddddc65c26c35d104343bc24c5576621fbf0
Ryan Riley <ryan.riley@panesofglass.org>
2011-06-10 22:11:04 -0700

    Added Request and Response message types.

--- src/lib/httpparser/Http.fs
+++ src/lib/httpparser/Http.fs
@@ -1,11 +1,41 @@
 ﻿module HttpParser.Http
 
 open System
+open System.Xml.Linq
 open FParsec.Primitives
 open FParsec.CharParsers
 open HttpParser.Primitives
 open HttpParser.CharParsers
 open HttpParser.Uri
+
+type HttpRequestMethod =
+  | GET
+  | POST
+  | PUT
+  | DELETE
+  | HEAD
+  | OPTIONS
+  // ... more to come
+
+type HttpVersion = HttpVersion of int * int
+
+type HttpRequestLine = HttpRequestLine of HttpRequestMethod * UriKind * HttpVersion
+
+type HttpResponseStatusLine = HttpResponseStatusLine of int * string
+
+type HttpHeader = HttpHeader of string * string list
+
+type HttpMessageBody =
+  | EmptyBody
+  | UrlFormEncodedBody of (string * string) list
+  | XmlBody of XElement
+  | JsonBody of string
+  | StringBody of string
+  | ByteArrayBody of byte[]
+
+type HttpMessage =
+  | HttpRequestMessage of HttpRequestLine * HttpHeader list * HttpMessageBody
+  | HttpResponseMessage of HttpResponseStatusLine * HttpHeader list * HttpMessageBody
 
 type HttpRequestMessageParseEvent =
   | RequestMessageBegin
