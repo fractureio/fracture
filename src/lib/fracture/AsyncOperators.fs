137c9a2c5e3ce232b035d7021257cb76bffdfed8
Ryan Riley <ryan.riley@panesofglass.org>
2011-06-10 22:10:17 -0700

    Added AsyncOperators and tests.

---
+++ src/lib/fracture/AsyncOperators.fs
@@ -0,0 +1,10 @@
+﻿module Fracture.Async
+
+let inline (>>=) m f = async.Bind(m, f)
+let inline (<*>) f m = f >>= fun f' -> m >>= fun m' -> async.Return(f' m')
+let inline pipe m f = m >>= fun m' -> async.Return(f m')
+let inline pipe2 x y f = async.Return f <*> x <*> y
+let inline pipe3 x y z f = async.Return f <*> x <*> y <*> z
+let inline (<!>) f m = pipe m f
+let inline ( *>) x y = pipe2 x y (fun _ z -> z)
+let inline ( <*) x y = pipe2 x y (fun z _ -> z)
