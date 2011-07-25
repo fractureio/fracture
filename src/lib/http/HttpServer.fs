5e0b0cc09f84f47fced3fac47c8665383d2b3adc
Dave Thomas <kukulcanenator@googlemail.com>
2011-07-25 01:27:53 +0100

    Temp experimental httpmachine parser

---
+++ src/lib/http/HttpServer.fs
@@ -0,0 +1,20 @@
+﻿module Fracture.HttpServer
+
+open System
+open System.Net
+open Fracture
+open Fracture.Common
+open HttpMachine
+open System.Collections.Generic
+open System.Diagnostics
+
+type HttpServer(headers, body) = 
+    let svr = TcpServer.Create(fun (data,svr,sd) -> 
+        let parserDelegate = ParserDelegate( (fun (a,b) ->headers(a,b,svr,sd)), (fun (data) -> (body(data, svr,sd))), (fun(req)-> ()) )
+        let parser = HttpParser(parserDelegate)
+        parser.Execute( new ArraySegment<_>(data))|> ignore)
+        
+    member h.Listen(port) =     
+        svr.Listen(port = port)
+
+    member h.Send= svr.Send
