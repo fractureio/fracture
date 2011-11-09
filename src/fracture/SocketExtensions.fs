323ff29e5cf491f05d076e2c283eef208f430464
Ryan Riley <ryan.riley@logos.com>
2011-11-08 22:51:01 -0800

    Simplify the invoke method in the SocketExtensions.

--- src/fracture/SocketExtensions.fs
+++ src/fracture/SocketExtensions.fs
@@ -6,19 +6,17 @@ open System.Net.Sockets
 
 /// Helper method to make Async calls easier.  InvokeAsyncMethod ensures the callback always
 /// gets called even if an error occurs or the Async method completes synchronously.
-let inline private invoke(asyncmethod, callback, args: SocketAsyncEventArgs) =
-    let result = asyncmethod args
-    if result <> true then 
-        callback args
+let inline private invoke(asyncMethod, callback, args: SocketAsyncEventArgs) =
+    if not (asyncMethod args) then callback args
 
 type Socket with 
     member s.AcceptAsyncSafe(callback, args) =
         invoke(s.AcceptAsync, callback, args) 
     member s.ReceiveAsyncSafe(callback, args) =
-        invoke(s.ReceiveAsync,callback, args) 
+        invoke(s.ReceiveAsync, callback, args) 
     member s.SendAsyncSafe(callback, args) =
-        invoke(s.SendAsync,callback, args) 
+        invoke(s.SendAsync, callback, args) 
     member s.ConnectAsyncSafe(callback, args) =
-        invoke(s.ConnectAsync,callback, args)
+        invoke(s.ConnectAsync, callback, args)
     member s.DisconnectAsyncSafe(callback, args) =
         invoke(s.DisconnectAsync, callback, args)
